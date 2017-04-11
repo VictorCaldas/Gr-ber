ccd7480b44828b28919dede364a013d968602956
victor <contatos.caldas@gmail.com>
2017-04-11 19:26:40 -0300

    Update method WB *incomplete*

--- Bohrmaschine.py
+++ Bohrmaschine.py
@@ -1,3 +1,4 @@
+import re
 from bs4 import BeautifulSoup
 from openpyxl import Workbook
 import requests
@@ -8,8 +9,7 @@ def cutting_pickingup_values():
     dict_values = {}
 
     # Taking the information from the page
-    response = requests.get("http://www.fnde.gov.br/pls/simad/internet_fnde.LIBERACOES_01_PC?p_ano=2017&p_programa"
-                            "=&p_uf=SP&p_municipio=120170")
+    response = requests.get("http://www.fnde.gov.br/pls/simad/internet_fnde.LIBERACOES_01_PC?p_ano=2017&p_programa=&p_uf=SP&p_municipio=120170")
 
     soup = BeautifulSoup(response.text, 'html.parser')
 
@@ -27,18 +27,17 @@ def cutting_pickingup_values():
 def create_wb():
     wb = Workbook()  # grab the active worksheet
     ws = wb.active
+    li =[['Entidade..: 44.518.405/0001-91 - PREF MUN DE ALVINLANDIA'], ['Município.: ALVINLANDIA - SP'], ['ALIMENTAÇÃO ESCOLAR - PROG.NACIONAL DE ALIMENTAÇÃO ESCOLAR'], ['Data Pgto'], ['OB'], ['Valor'], ['Programa'], ['Banco'], ['Agência'], ['C/C'], ['08/MAR/2017'], ['802989'], ['212,00'], ['PNAE - Alimentação Escolar - AEE'], ['BANCO DO BRASIL'], ['6877'], ['0000169374'], ['08/MAR/2017'], ['802990'], ['454,40'], ['PNAE - Alimentação Escolar - EJA'], ['BANCO DO BRASIL'], ['6877'], ['0000169374'], ['08/MAR/2017'], ['802991'], ['979,20'], ['PNAE - Alimentação Escolar - Ensino Médio'], ['BANCO DO BRASIL'], ['6877'], ['0000169374'], ['14/MAR/2017'], ['803132'], ['2.610,80'], ['PNAE - Alimentação Escolar - Creche'], ['BANCO DO BRASIL'], ['6877'], ['0000169374'], ['14/MAR/2017'], ['803140'], ['3.168,00'], ['PNAE - Alimentação Escolar -Ensino Fundamental'], ['BANCO DO BRASIL'], ['6877'], ['0000169374'], ['14/MAR/2017'], ['803145'], ['1.267,40'], ['PNAE - Alimentação Escolar - Pré-escola.'], ['BANCO DO BRASIL'], ['6877'], ['0000169374'], ['05/ABR/2017'], ['804810'], ['3.168,00'], ['PNAE - Alimentação Escolar -Ensino Fundamental'], ['BANCO DO BRASIL'], ['6877'], ['0000169374'], ['05/ABR/2017'], ['804815'], ['979,20'], ['PNAE - Alimentação Escolar - Ensino Médio'], ['BANCO DO BRASIL'], ['6877'], ['0000169374'], ['05/ABR/2017'], ['804835'], ['454,40'], ['PNAE - Alimentação Escolar - EJA'], ['BANCO DO BRASIL'], ['6877'], ['0000169374'], ['05/ABR/2017'], ['804839'], ['212,00'], ['PNAE - Alimentação Escolar - AEE'], ['BANCO DO BRASIL'], ['6877'], ['0000169374'], ['05/ABR/2017'], ['805005'], ['1.267,40'], ['PNAE - Alimentação Escolar - Pré-escola.'], ['BANCO DO BRASIL'], ['6877'], ['0000169374'], ['05/ABR/2017'], ['805040'], ['2.610,80'], ['PNAE - Alimentação Escolar - Creche'], ['BANCO DO BRASIL'], ['6877'], ['0000169374'], ['07/ABR/2017'], ['805165'], ['4.160,60'], ['MAIS EDUCAÇÃO - Fundamental'], ['BANCO DO BRASIL'], ['6877'], ['0000169374'], ['Total:'], ['21.544,20'], ['']]
 
-    # Data can be assigned directly to cells
-    ws['A1'] = 42
 
-    # Rows can also be appended
-    ws.append([1, 2, 3])
-
-    # Python types will automatically be converted
-    ws['A2'] = datetime.datetime.now()
     today = datetime.datetime.today()
     nome = 'Planilha{:02d}-{:02d}-{}.xlsx'.format(today.day, today.month, today.year)
     # Save the file
+    for row, val in enumerate(li, start=1):
+        if val == "Entidade":
+            print("achou")
+        ws.cell(row=row, column=1).value = val
+
     wb.save(nome)
 
 if __name__ == "__main__":
--- Schaufel.py
+++ Schaufel.py
@@ -1,4 +1,3 @@
-
 import requests
 import time
 from bs4 import BeautifulSoup
@@ -6,7 +5,7 @@ from Bohrmaschine import cutting_pickingup_values
 from money import Money
 
 TIME = 0
-teste = 0
+controle = 0
 te = 0.0
 
 # get cookies
@@ -15,7 +14,7 @@ mySession.get("http://www.fnde.gov.br/pls/simad/internet_fnde.liberacoes_01_pc")
 
 # Get values for all places
 dict_values = cutting_pickingup_values()
-
+li = []
 for key in dict_values.keys():
     data = [
         ('p_ano', '2017'),
@@ -37,15 +36,16 @@ for key in dict_values.keys():
     soup = BeautifulSoup(preview.text, 'html.parser')
     alltables = soup.findAll("table", attrs={"border": "1", "width": "100%"})
 
+
     for table in alltables:
         rows = table.findAll('tr')
         for tr in rows:
             cols = tr.findAll('td')
             print("   ")
             for td in cols:
-                print(td.text.strip(), end='\t'*3)
-
-                if teste == 1:
+                print(td.text.strip(), end='\t' * 3)
+                li.append(td.text.strip())
+                if controle == 1:
                     value = td.text.strip().replace(".", "").replace(",", ".")
                     value = float(value)
                     te = value * 10
@@ -56,13 +56,19 @@ for key in dict_values.keys():
                     m = Money(te, 'BRL')
                     print(m)
 
+                    li.append("Total 2017: ")
+                    li.append(m)
                     print('\n')
                     print("Agricultura Familiar: ", end='')
-                    print(ty)
-                    teste = 0
-                if td.text.strip() == "Total:":
-                    teste = 1
 
-    print('\n'"---------------------------------------------------------------")
+                    li.append("Agricultura Familiar: ")
+                    li.append(ty)
+                    print(ty)
+                    controle = 0
+                if td.text.strip() == "Total:":
+                    controle = 1
+    print('\n'"-----------------------------------------------------------------------------")
     time.sleep(TIME)
 print('\n'"Fim do Relatório")
+for i in li:
+    print(i)
