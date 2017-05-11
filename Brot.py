549c179227d7f3a49ff0e2f7fd53ed90fa75ad96
victor <contatos.caldas@gmail.com>
2017-05-10 22:44:16 -0300

    Update crawler

---
+++ Brot.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+import os
+
+from flask import Flask, request
+from Schaufel import report_fnde
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def began():
+    if request.method == 'GET':
+        report_fnde()
+        return "Projeto Lilian! - Fim do Relatório"
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
--- Schaufel.py
+++ Schaufel.py
@@ -1,74 +1,106 @@
 import requests
 import time
+import datetime
 from bs4 import BeautifulSoup
-from Bohrmaschine import cutting_pickingup_values
 from money import Money
-
-TIME = 0
-controle = 0
-te = 0.0
-
-# get cookies
-mySession = requests.session()
-mySession.get("http://www.fnde.gov.br/pls/simad/internet_fnde.liberacoes_01_pc")
-
-# Get values for all places
-dict_values = cutting_pickingup_values()
-li = []
-for key in dict_values.keys():
-    data = [
-        ('p_ano', '2017'),
-        ('p_verifica', 'sigef'),
-        ('p_programa', 'C7'),
-        ('p_cgc', ''),
-        ('p_uf', 'SP'),
-        ('p_municipio', key),
-        ('p_tp_entidade', '02'),
-    ]
-
-    # Create preview
-    preview = mySession.post('http://www.fnde.gov.br/pls/simad/internet_fnde.liberacoes_result_pc', data=data)
-    a = open("preview.html", "w")
-    a.write(preview.text)
-    a.close()
-
-    # Magic Baby
-    soup = BeautifulSoup(preview.text, 'html.parser')
-    alltables = soup.findAll("table", attrs={"border": "1", "width": "100%"})
+from openpyxl import Workbook
 
 
-    for table in alltables:
-        rows = table.findAll('tr')
-        for tr in rows:
-            cols = tr.findAll('td')
-            print("   ")
-            for td in cols:
-                print(td.text.strip(), end='\t' * 3)
-                li.append(td.text.strip())
-                if controle == 1:
-                    value = td.text.strip().replace(".", "").replace(",", ".")
-                    value = float(value)
-                    te = value * 10
-                    ttt = te * 30
-                    ty = ttt / 100
-                    print('\n')
-                    print("Total 2017: ", end='')
-                    m = Money(te, 'BRL')
-                    print(m)
+def cutting_pickingup_values():
+    dict_values = {}
 
-                    li.append("Total 2017: ")
-                    li.append(m)
-                    print('\n')
-                    print("Agricultura Familiar: ", end='')
+    # Taking the information from the page
+    response = requests.get("http://www.fnde.gov.br/pls/simad/internet_fnde.LIBERACOES_01_PC?p_ano=2017&p_programa"
+                            "=&p_uf=SP&p_municipio "
+                            "=120170")
 
-                    li.append("Agricultura Familiar: ")
-                    li.append(ty)
-                    print(ty)
-                    controle = 0
-                if td.text.strip() == "Total:":
-                    controle = 1
-    print('\n'"-----------------------------------------------------------------------------")
-    time.sleep(TIME)
-print('\n'"Fim do Relatório")
-for i in li:
-    print(i)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Cutting information and get Values
+    select_municipio = soup.find("select", attrs={"name": "p_municipio"})
+    values_municipio = select_municipio.find_all("option")
+
+    for option in values_municipio:
+        ('value: {}, text: {}'.format(option['value'], option.text))
+        dict_values[option['value']] = option.text
+
+    return dict_values
+
+
+def report_fnde():
+    delay = 0
+    control = 0
+
+    # get cookies
+    mySession = requests.session()
+    mySession.get("http://www.fnde.gov.br/pls/simad/internet_fnde.liberacoes_01_pc")
+
+    # Get values for all places
+    dict_values = cutting_pickingup_values()
+    li = []
+    for key in dict_values.keys():
+        data = [
+            ('p_ano', '2017'),
+            ('p_verifica', 'sigef'),
+            ('p_programa', 'C7'),
+            ('p_cgc', ''),
+            ('p_uf', 'SP'),
+            ('p_municipio', key),
+            ('p_tp_entidade', '02'),
+        ]
+
+        # Create preview
+        preview = mySession.post('http://www.fnde.gov.br/pls/simad/internet_fnde.liberacoes_result_pc', data=data)
+        a = open("preview.html", "w")
+        a.write(preview.text)
+        a.close()
+
+        # Magic Baby
+        soup = BeautifulSoup(preview.text, 'html.parser')
+        alltables = soup.findAll("table", attrs={"border": "1", "width": "100%"})
+
+        # excell - failed :(
+        wb = Workbook()  # grab the active worksheet
+        ws = wb.active
+        today = datetime.datetime.today()
+        nome = 'Planilha{:02d}-{:02d}-{}.xlsx'.format(today.day, today.month, today.year)
+
+        for table in alltables:
+            rows = table.findAll('tr')
+            for tr in rows:
+                cols = tr.findAll('td')
+                print("   ")
+                for td in cols:
+                    print(td.text.strip(), end='\t' * 3)
+                    li.append(td.text.strip())
+                    if control == 1:
+                        value = td.text.strip().replace(".", "").replace(",", ".")
+                        value = float(value)
+                        a = value * 10
+                        b = a * 30
+                        value_agricultura_familiar = b / 100
+                        print('\n')
+                        print("Total 2017: ")
+                        value_total_2017 = Money(a, 'BRL')
+                        print(str(value_total_2017).replace("BRL", "R$"))
+                        li.append("Total 2017: ")
+                        li.append(value_total_2017)
+                        print('\n')
+                        print("Agricultura Familiar: ")
+                        li.append("Agricultura Familiar: ")
+                        li.append(value_agricultura_familiar)
+                        print(str(Money(value_agricultura_familiar, 'BRL')).replace("BRL", "R$"))
+                        control = 0
+                    if td.text.strip() == "Total:":
+                        control = 1
+        print('\n'"-----------------------------------------------------------------------------")
+        time.sleep(delay)
+    print('\n'"Fim do Relatório")
+
+    # wb.save(nome)
+    # for i in li:
+    # print(i)
+
+
+if __name__ == "__main__":
+    report_fnde()
